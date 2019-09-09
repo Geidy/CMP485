3baa96459bb047cab0a4bc2448af89f73e373703
Geidy <geidydm@gmail.com>
2019-09-09 07:11:34 -0400

    Data Types

---
+++ CMP485/src/Question_1.java
@@ -0,0 +1,21 @@
+
+public class Question_1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		//Data Types
+		double x = 5.0/2;
+		double y = 10 % 3; //exact #
+		int a = 10 % 3;  //without decimal points
+		int z = 3 / 3 + 5 * 3;
+		double b = 2 / 3; //right answer 
+		int c = 2 / 3; //Answer will be 0
+	 /*   “Comp” + ”Sci”; //__char
+		“10” + ”-1”		__char
+		“Belcalis Marlenis Almanzar”.charAt(2)
+		String elem = “hydrogen”;  
+		elem.length(); _int with the size of the String
+*/
+	}
+
+}
