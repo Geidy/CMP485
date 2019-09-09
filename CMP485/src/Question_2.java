11dc3f13183ed2ab088a1f11c63ea8ea24882461
Geidy <geidydm@gmail.com>
2019-09-09 07:31:05 -0400

    Question 2

---
+++ CMP485/src/Question_2.java
@@ -0,0 +1,33 @@
+
+public class Question_2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		
+		String flavor = "vanilla";
+		double price;
+		
+		switch(flavor) {
+		case "stawberry":
+		case "chocolate":
+			price = 3.99;
+			break;
+		case "vanilla":
+			price = 3.49;
+			break;
+			default:
+				price = 0.0;
+		}
+		
+		/* if picks flavor strawberry the price would be $3.99
+		 * if picks flavor pistachio the price would be 0.0 
+		 * 
+		 * 
+		 */
+		
+		
+
+	}
+
+}
