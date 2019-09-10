0730e309e5206807ef6b8705979ac7d1c5bd6099
Geidy <geidydm@gmail.com>
2019-09-10 00:45:27 -0400

    Question3

---
+++ CMP485/src/Question_3.java
@@ -0,0 +1,16 @@
+
+public class Question_3 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		//Write a condition that is only true if an integer is divided by 4 or 7
+		int x;
+		if (x % 4 == 0 & x % 7 == 0) {
+			System.out.println(x);
+		}
+			
+			
+
+	}
+
+}
