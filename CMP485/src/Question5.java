f6099d96b024832fac79a2feb089c8686aada0be
Geidy <geidydm@gmail.com>
2019-09-10 01:30:53 -0400

    Question 5

---
+++ CMP485/src/Question5.java
@@ -0,0 +1,16 @@
+
+public class Question5 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		//Create a Loop that prints the cubes
+		//(x^3) of all integers from -10 to 10
+		
+		for(int i = -10; i<=10; i++) {
+			i = (i^3);
+			System.out.println(i);
+		}
+
+	}
+
+}
