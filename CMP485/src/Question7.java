0a72c63d5ec037540e98247e78d20c982b8fdf7c
Geidy <geidydm@gmail.com>
2019-09-10 09:23:37 -0400

    Question 7

---
+++ CMP485/src/Question7.java
@@ -0,0 +1,43 @@
+
+public class Question7 {
+
+	public static String mysteryFn(String[] input) {
+		// TODO Auto-generated method stub
+		String output = " ";
+		for (int i = 0; i < input.length(); i++) {
+			if(
+				input.charAt(i) =='a' || input.charAt(i) =='e';
+				input.charAt(i) =='i' || input.charAt(i) =='o';
+				input.charAt(i) =='u';
+				)
+			}
+			continue
+	}
+	output += input.CharAt(i);
+}
+return output;
+			
+			
+		}
+	
+	
+	/*What is the return type?
+	 * char, String
+	 * 
+	 * What is the parameter type ?
+	 * char
+	 * 
+	 * What does it do?
+	 * Find the vowels in a sentence
+	 * 
+	 * What is the result of mysteryFn(“Woah, we're half way there”)
+	 * oaeeaee
+	 * 
+	 *  Is there a case that it does not solve correctly?
+	 *  I'm not sure
+	 * 
+	 */
+
+	}
+
+}
