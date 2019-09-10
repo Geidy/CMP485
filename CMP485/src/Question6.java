3862a735bf52a9bcead33e2a9a6c51f7c1dcd2ff
Geidy <geidydm@gmail.com>
2019-09-10 08:56:35 -0400

    Question 6

---
+++ CMP485/src/Question6.java
@@ -0,0 +1,31 @@
+
+public class Question6 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		/*What's the output of this snippet
+		 *  
+		 */
+		float score = 0;
+		while (score < 1) {
+			float score += .1;
+			System.out.println(score);
+			if(score == .5) {
+				break;
+			}
+		}
+		/*output will be 
+		 * .1
+		 * .2
+		 * .3
+		 * .4
+		 * .6
+		 * .7
+		 * .8
+		 * .9
+		 * 
+		 */
+
+	}
+
+}
