2e9cb3d8afeec16bf23195584a3c377de326c6a2
Geidy <geidydm@gmail.com>
2019-09-10 01:40:33 -0400

    Question 5 modified

--- CMP485/src/Question5.java
+++ CMP485/src/Question5.java
@@ -6,8 +6,8 @@ public class Question5 {
 		//Create a Loop that prints the cubes
 		//(x^3) of all integers from -10 to 10
 		
-		for(int i = -10; i<=10; i++) {
-			i = (i^3);
+		for(double i = -10; i<=10; i++) {
+			i = Math.pow(i, 3);
 			System.out.println(i);
 		}
 
