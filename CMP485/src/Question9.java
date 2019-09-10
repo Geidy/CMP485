3a9587d5da1ddb19bb6206ba241ec2b094423ec4
Geidy <geidydm@gmail.com>
2019-09-10 10:35:29 -0400

    Question9

---
+++ CMP485/src/Question9.java
@@ -0,0 +1,19 @@
+
+public class Question9 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		/*
+		 * Write a code snippet that prints the values 
+		 * in an array backwards
+		 */
+		
+		char [] flags = {'c', 'f','l','b','a'};
+		
+		for (int i=flags[4]; i<=flags.length; i-- ) {
+			System.out.println(i);
+		}
+
+	}
+
+}
