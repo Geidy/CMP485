849a4408bcfebf88c8900d5258c17f1cab362aba
Geidy <geidydm@gmail.com>
2019-09-10 10:59:10 -0400

    Question 10

---
+++ CMP485/src/Question10.java
@@ -0,0 +1,18 @@
+
+public class Question10 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		//Write a code that pints out the 3rd row of the 2d array
+		String [][] classroom = new String [10][5];
+		
+		for(char i; i==3; i++) {
+			for(char j =0;j <=4; j++ ) {
+				System.out.println(i + j);
+			}
+			
+		}
+
+	}
+
+}
